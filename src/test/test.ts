01ced17c93bd810cc292dd65a478d098d0326532
Justin Day <justin@unsmoke.me>
2019-05-31 13:39:59 -0400

    Added initial test file and .gitignore

---
+++ src/test/test.ts
@@ -0,0 +1,14 @@
+/**
+ * @file Main test file for Node Site Init.
+ */
+
+// tslint:disable-next-line:import-name
+import test from 'ava';
+
+test('General config is working', async (t) => {
+  t.pass();
+});
+
+test('General config is working', async (t) => {
+
+});
