75b1f6e2933f2b4fc5689023b65e5c8ad032cb58
Justin Day <justin@unsmoke.me>
2019-10-24 11:31:00 -0400

    Added index main file.

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,3 +1,7 @@
 /**
  * @file Main entry point for module.
  */
+import Module from './classes/Module';
+declare const module: Module;
+export default module;
+export { Module };
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@
  * @file Main entry point for module.
  */
 
+import Module from './classes/Module';
+
+const module = new Module();
+export default module;
+export { Module };
